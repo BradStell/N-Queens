f9717f586d83a69bf3cc68de0a9107c86956121c
Brad <bradstell87@gmail.com>
2015-09-30 18:30:54 -0400

    Main file setup with header

---
+++ main.cpp
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////////
+//																				//
+//   Author:	Brad Stell														//
+//																				//
+//	  Date:		9/30/2015														//
+//																				//
+//	  Language:		c++															//
+//																				//
+//	  Description:																//
+//		This program will implement 'Random-Restart Hill Climbing' algorithm	//
+//		to solve the N-Queens problem. The N-Queens problem has an N x N board	//
+//		with N queens placed randomly on the board. The goal of the game is		//
+//		to place the queens in such a manner so that there is no conflicts		//
+//		between queens. Thus, no queen shall be in the same row, column or		//
+//		diagonal as any other queen.											//
+//																				//
+//////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "This will be the N-Queens problem Solved with random "
+		<< "restart hill climbing algorithm."
+		<< std::endl;
+
+	return 0;
+}
